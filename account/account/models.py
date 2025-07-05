c757a4281aba8a2159fc08307bebe4c44b5d82ec
USMAN <mirzausmanmu07842@gmail.com>
2025-07-05 14:34:38 +0500

    added models

--- account/account/models.py
+++ account/account/models.py
@@ -1,1 +1,16 @@
-from
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+    date_of_birth = models.DateField(blank=True,  null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+    
